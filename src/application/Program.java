ac8805bc95baa312e80796187a2f1ea66ebeef00
JayCesar <batistajulio215@gmail.com>
2023-10-12 16:13:33 -0300

    Pipeline

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -10,23 +11,19 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		
-		Stream<Integer> st1 = list.stream().map(p -> p * 10);
-		
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
-		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		// Elemento inicial (neutro) e depois uma função de dois argumentos e um resultado 
+		System.out.println("Sum = " + sum);
 		
-		System.out.println(Arrays.toString(st2.toArray()));
 		
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
-		// Primeiro elemento 0
-		// Segundo é a função que vai gerar a stream
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
-		// Esse limit é uma short-circuit
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
 		
-		// Sequêcia de fibonacci
-		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);
-		System.out.println(Arrays.toString(st4.limit(10).toArray()));
-		
+		System.out.println(Arrays.toString(newList.toArray()));
 	}
 }
