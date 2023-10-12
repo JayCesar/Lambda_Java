067b6762ea7c76a584ce3ac4a2049d2ba14ac0e1
JayCesar <batistajulio215@gmail.com>
2023-10-12 14:30:18 -0300

    FUNCTION: Implementação a Interface (usando Stream)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -19,10 +20,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// Consumer: Expressão Lambda Inline
-		double factor = 1.1;
-		list.forEach(p -> p.setPrice(p.getPrice() * factor)); 
-		list.forEach(System.out::println); // Outra foram de imprimir
-		
+		// FUNCTION: Implementação a Interface (usando Stream)
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		// A função map aplica uma função pra cada elemento da stream, gerando uma nova stream com os elementos transformados
+		// O map alica a função que tiver dentro dela pra cada elemento da lista
+		names.forEach(System.out::println);
 	}
 }
