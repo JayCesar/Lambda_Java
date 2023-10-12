4261d0bd2c869945785a6c1a72c70f177a7b4c94
JayCesar <batistajulio215@gmail.com>
2023-10-12 15:04:03 -0300

    FUNCAO(FUNCAO): Criando função que aceita função como argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import util.ProductService;
 
 public class Program {
 
@@ -20,8 +19,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// FUNCTION: Expressão Lambada Inline
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-		names.forEach(System.out::println);
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+		
 	}
 }
