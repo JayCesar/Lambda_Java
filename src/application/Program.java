e3c298bfabba600b84d924b936a51c53d6716da5
JayCesar <batistajulio215@gmail.com>
2023-10-12 12:20:05 -0300

    Predicate: Expressão Lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -18,8 +19,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// Referêncîa com método não estático
-		list.removeIf(Product::nonStaticProductPredicate);
+		// Expressão Lambda declarada
+		double min = 100.0;
+		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		list.removeIf(pred);
 		list.forEach(p -> System.out.println(p));
 		
 	}
