94106f90ed6b7c3f40d006a5e2bc42f53979c890
JayCesar <batistajulio215@gmail.com>
2023-10-12 11:23:32 -0300

    Expressão Lambda como argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,13 +15,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		// Agora eu criei uma expressão lambda
-		// Aqui eu defini uma função anônima (arrow function) - de um lado vem os parâmetros e no lado a implementação da função
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-	
-		// Meu com será uma fnção que receberá dois  argumentos p1 e p2 e a implementação é o que estará entre as chaves;
-	
-		list.sort(comp);
+		// Adicionei como arumento dentro do sort
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
 		for (Product p : list) System.out.println(p);
 		
